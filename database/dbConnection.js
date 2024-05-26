df0f3aad3fa8bba05e0e96e84ffcdb2055686a52
Shahzaib55555 <30041@students.riphah.edu.pk>
2024-05-26 04:00:05 -0700

    Update dbConnection.js

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -2,8 +2,10 @@ import mongoose from "mongoose";
 
 export const dbConnection = () => {
   console.log('process.env.MONGO_URI :>> ', process.env.MONGO_URI);
+  const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
   mongoose
-    .connect('mongodb+srv://jobseeker:jobseeker@cluster0.y1glntk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+    .connect('mongodb+srv://${username}:${password}@cluster0.y1glntk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
       dbName: "MERN_JOB_SEEKING_WEBAPP",
     })
     .then(() => {
