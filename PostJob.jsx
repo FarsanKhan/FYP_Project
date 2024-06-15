7de7a86a5a312910bdeab8cc6b8947e0d6c2110e
Shahzaib55555 <30041@students.riphah.edu.pk>
2024-06-16 01:00:35 +0500

    Add files via upload

---
+++ PostJob.jsx
@@ -0,0 +1,206 @@
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { Context } from "../../main";
+const PostJob = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [country, setCountry] = useState("");
+  const [city, setCity] = useState("");
+  const [location, setLocation] = useState("");
+  const [jobtype, setjobtype] = useState("");
+  const [salaryFrom, setSalaryFrom] = useState("");
+  const [salaryTo, setSalaryTo] = useState("");
+  const [fixedSalary, setFixedSalary] = useState("");
+  const [salaryType, setSalaryType] = useState("default");
+  const [image, setImage] = useState("");
+
+  const { isAuthorized, user } = useContext(Context);
+
+  const handleJobPost = async (e) => {
+    e.preventDefault();
+    if (salaryType === "Fixed Salary") {
+      setSalaryFrom("");
+      setSalaryFrom("");
+    } else if (salaryType === "Ranged Salary") {
+      setFixedSalary("");
+    } else {
+      setSalaryFrom("");
+      setSalaryTo("");
+      setFixedSalary("");
+    }
+    await axios
+      .post(
+        "http://localhost:4000/api/v1/job/post",
+        fixedSalary.length >= 4
+          ? {
+              title,
+              description,
+              category,
+              country,
+              city,
+              location,
+              jobtype,
+              fixedSalary,
+              image,
+            }
+          : {
+              title,
+              description,
+              category,
+              country,
+              city,
+              location,
+              jobtype,
+              salaryFrom,
+              salaryTo,
+              image,
+            },
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((res) => {
+        toast.success(res.data.message);
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message);
+      });
+  };
+
+  const navigateTo = useNavigate();
+  if (!isAuthorized || (user && user.role !== "Employer")) {
+    navigateTo("/");
+  }
+  return (
+    <>
+      <div className="job_post page">
+        <div className="container">
+          <h3>POST NEW JOB</h3>
+          <form onSubmit={handleJobPost}>
+            <div className="wrapper">
+              <input
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Job Title"
+              />
+              <select
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              >
+                <option value="">Select Category</option>
+                <option value="Information Technology">Information Technology(IT)</option>
+                <option value="HealthCare">
+                  Healthcare
+                </option>
+                <option value="Agriculture">
+                  Agriculture
+                </option>
+                <option value="Restaurants">
+                Restaurants
+                </option>
+                <option value="Account & Finance">Account & Finance</option>
+                <option value="Engineering">
+                Engineering
+                </option>
+                <option value="Tourism">Tourism</option>
+                <option value="Media">
+                  Media
+                </option>
+                </select>
+            </div>
+            <div className="wrapper">
+              <input
+                type="text"
+                value={country}
+                onChange={(e) => setCountry(e.target.value)}
+                placeholder="Country"
+              />
+              <input
+                type="text"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+                placeholder="City"
+              />
+            </div>
+            <input
+              type="text"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+              placeholder="Location"
+            />
+    <div className="salary_wrapper">
+     <select
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              >
+                <option value="">Select job type</option>
+                <option value="Part-time"> Part-time</option>
+                <option value="Full-time"> Full-time</option>
+                <option value="Internship">Internship </option>
+                </select>
+                </div>
+
+            <div className="salary_wrapper">
+              <select
+                value={salaryType}
+                onChange={(e) => setSalaryType(e.target.value)}
+              >
+                <option value="default">Select Salary Type</option>
+                <option value="Fixed Salary">Fixed Salary -Pkr</option>
+                <option value="Ranged Salary">Ranged Salary</option>
+              </select>
+              <div>
+                {salaryType === "default" ? (
+                  <p>Please provide Salary Type *</p>
+                ) : salaryType === "Fixed Salary" ? (
+                  <input
+                    type="number"
+                    placeholder="Enter Fixed Salary (PKR)"
+                    value={fixedSalary}
+                    onChange={(e) => setFixedSalary(e.target.value)}
+                  />
+                ) : (
+                  <div className="ranged_salary">
+                    <input
+                      type="number"
+                      placeholder="Salary From (PKR)"
+                      value={salaryFrom}
+                      onChange={(e) => setSalaryFrom(e.target.value)}
+                    />
+                    <input
+                      type="number"
+                      placeholder="Salary To (PKR)"
+                      value={salaryTo}
+                      onChange={(e) => setSalaryTo(e.target.value)}
+                    />
+                  </div>
+                )}
+              </div>
+            </div>
+            <textarea
+              rows="10"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              placeholder="Job Description"
+            />
+               <input
+                      type="file"
+                      onChange={(e) => setImage(e.target.files[0])} //event target files ye complete aik array huta hai...for multiple file remove 0
+                    />
+
+            <button type="submit">Create Job</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PostJob;
