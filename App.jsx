f53ceaf9e89540a83f0539c1bc21024d2581793d
Shahzaib55555 <30041@students.riphah.edu.pk>
2024-10-30 06:53:56 -0700

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,64 +1,28 @@
-import React, { useContext, useEffect } from "react";
-import "./App.css";
-import { Context } from "./main";
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./components/Auth/Login";
-import Register from "./components/Auth/Register";
+import Authentication from "./components/Auth/Authentication";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
-import axios from "axios";
-import Navbar from "./components/Layout/Navbar";
-import Footer from "./components/Layout/Footer";
-import Home from "./components/Home/Home";
-import Jobs from "./components/Job/Jobs";
-import JobDetails from "./components/Job/JobDetails";
-import Application from "./components/Application/Application";
-import MyApplications from "./components/Application/MyApplications";
-import PostJob from "./components/Job/PostJob";
-import NotFound from "./components/NotFound/NotFound";
-import MyJobs from "./components/Job/MyJobs";
+import "./index.css";
+import AppRoutes from "./components/AppRoutes";
+import Landing from "./components/Home/Landing";
 
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { staleTime: 10000 } },
+});
 
 const App = () => {
-  const { isAuthorized, setIsAuthorized, setUser } = useContext(Context);
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await axios.get(
-          "http://localhost:4000/api/v1/user/getuser",
-          {
-            withCredentials: true,
-          }
-        );
-        setUser(response.data.user);
-        setIsAuthorized(true);
-      } catch (error) {
-        setIsAuthorized(false);
-      }
-    };
-    fetchUser();
-  }, [isAuthorized]);
-
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <Navbar />
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/job/getall" element={<Jobs />} />
-          <Route path="/job/:id" element={<JobDetails />} />
-          <Route path="/application/:id" element={<Application />} />
-          <Route path="/applications/me" element={<MyApplications />} />
-          <Route path="/job/post" element={<PostJob />} />
-          <Route path="/job/me" element={<MyJobs />} />
-          <Route path="*" element={<NotFound />} />
-         
+          <Route path="/" element={<Landing />} />
+          <Route path="/*" element={<Authentication />} />
+          <Route path="/app/*" element={<AppRoutes />} />
         </Routes>
-        <Footer />
         <Toaster />
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 };
 
